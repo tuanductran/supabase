e2b4d19abc5ba1d1dae3bd848fda816bcff4adf0
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-02-17 18:20:55 -0500

    chore: update auto API docs

--- studio/components/to-be-cleaned/DisplayProjectSettings.tsx
+++ studio/components/to-be-cleaned/DisplayProjectSettings.tsx
@@ -2,7 +2,7 @@ import useSWR from 'swr'
 import { FC } from 'react'
 import { useRouter } from 'next/router'
 import { get } from 'lib/common/fetch'
-import { IconAlertCircle, Input, Loading, Typography } from '@supabase/ui'
+import { Badge, IconAlertCircle, Input, Loading, Typography } from '@supabase/ui'
 
 import { API_URL } from 'lib/constants'
 import Panel from './Panel'
@@ -61,15 +61,17 @@ export const DisplayApiSettings = () => {
               label={
                 <>
                   {x.tags?.split(',').map((x: any, i: number) => (
-                    <code key={`${x}${i}`} className="text-xs bg-gray-500 text-white px-2">
+                    <code key={`${x}${i}`} className="text-xs  text-white px-2">
                       {x}
                     </code>
                   ))}
                   {x.tags === 'service_role' && (
-                    <code className="text-xs bg-red-500 px-2 ml-1 text-white">{'secret'}</code>
+                    <>
+                      <code className="text-xs bg-red-900 px-2 ml-1 text-white">{'secret'}</code>
+                    </>
                   )}
                   {x.tags === 'anon' && (
-                    <code className="text-xs bg-gray-500 text-white px-2 ml-1">{'public'}</code>
+                    <code className="text-xs text-white px-2 ml-1">{'public'}</code>
                   )}
                 </>
               }
